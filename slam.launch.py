7d0c1aabb5ab46e48eaeb120eed4cedfced9af61
Mustafa-s3 <mustafa.sha2001@gmail.com>
2026-06-17 03:21:54 +0300

    Update slam.launch.py

--- slam.launch.py
+++ slam.launch.py
@@ -1,15 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration 
 
 def generate_launch_description():
     sllidar_pkg = get_package_share_directory('sllidar_ros2')
-    slam_toolbox_pkg = get_package_share_directory('slam_toolbox')
+    # slam_toolbox_pkg = get_package_share_directory('slam_toolbox') # Commented out for static nav
     xacro_file = "/home/ubuntu/hariri_ws/src/robot_description/urdf/robot.urdf.xacro"
+
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    map_yaml_file = LaunchConfiguration('map', default='/home/ubuntu/my_map.yaml')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    params_file = LaunchConfiguration('params_file', default=os.path.join(
+        nav2_bringup_dir, 'params', 'nav2_params.yaml'))
     
     # Use xacro command to generate XML
     robot_description_content = Command(['xacro ', xacro_file])
@@ -17,8 +23,21 @@ def generate_launch_description():
     # Create the parameters dictionary
     params = {'robot_description': robot_description_content}
 
+    # Defining the Nav2 bringup launch file
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
+        ),
+        launch_arguments={
+            'map': map_yaml_file,
+            'use_sim_time': use_sim_time,
+            'params_file': params_file,
+            'autostart': 'true'
+        }.items()
+    )
+
     return LaunchDescription([
-        # LiDAR (using its own launch file)
+        # LiDAR
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(sllidar_pkg, 'launch', 'sllidar_c1_launch.py')
@@ -33,19 +52,14 @@ def generate_launch_description():
             output='screen'
         ),
         
-        # Node: Robot State Publisher
+        # Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
             parameters=[params]
         ),
-        
-        # SLAM Toolbox (using its online async launch file)
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(slam_toolbox_pkg, 'launch', 'online_async_launch.py')
-            )
-            
-        ),
+
+        # Nav2 bringup
+        nav2_bringup_launch
     ])
